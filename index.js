b0ab59ac98f1134a0fb5c29b33dcd989282c08f5
YuSetiawan <ysfstiawan@gmail.com>
2023-08-29 10:23:27 +0700

    delete JOI in products

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mainRouter = require('./src/routers/index');
-const port = 3000;
+const port = 4000;
 
 app.use(express.json());
 app.use(cors());
