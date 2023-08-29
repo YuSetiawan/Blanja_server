e469e428c52a539374de56deff6c7548511c14b2
YuSetiawan <ysfstiawan@gmail.com>
2023-08-30 01:26:06 +0700

    revised photo user

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../middlewares/upload');
 const userController = require('../controller/users');
 
 router
   .get('/', userController.user)
   .get('/profile', userController.profile)
   .get('/:id', userController.getselectUsers)
-  .put('/profile/:id', userController.updateImg)
+  .put('/profile/:id', upload, userController.updateImg)
   .post('/register', userController.register)
   .post('/registerSeller', userController.registerSeller)
   .post('/login', userController.login)
